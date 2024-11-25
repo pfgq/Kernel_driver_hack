58093b4c922efd73980920815c654eeffda82862
pfgq <pfgq@qq.com>
2024-11-26 00:24:33 +0800

    Update process.c

--- kernel/process.c
+++ kernel/process.c
@@ -10,7 +10,6 @@
 extern struct mm_struct *get_task_mm(struct task_struct *task);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
-extern void mmput(struct mm_struct *);
 #endif
 
 uintptr_t get_module_base(pid_t pid, char *name)
@@ -38,7 +37,6 @@ uintptr_t get_module_base(pid_t pid, char *name)
 	{
 		return false;
 	}
-	mmput(mm);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
 	vma_iter_init(&vmi, mm, 0);
